2eb771302af4bfbd2f0dc6b172316012d8da5153
pcm9909 <pcm9909@naver.com>
2024-07-13 17:33:59 +0900

    [feat] Add header

---
+++ ex03/HumanA.hpp
@@ -0,0 +1,13 @@
+#pragma once 
+#include "Weapon.hpp"
+
+class HumanA
+{
+    public:
+        HumanA(std::string name, Weapon &weapon);
+        ~HumanA();
+        void attack();
+    private:
+        std::string _name;
+        Weapon &_weapon;
+};
