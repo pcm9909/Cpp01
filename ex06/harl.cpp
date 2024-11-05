c831af9b6faba1ceb2b60f2bc2fe3cf89bf993ba
pcm9909 <pcm9909@naver.com>
2024-11-05 15:28:16 +0900

    done

--- ex06/harl.cpp
+++ ex06/harl.cpp
@@ -16,33 +16,31 @@ void Harl::complain(std::string level)
     std::cout << "does not exist" << std::endl;
 }
 
-Harl::Harl()
-{
-    std::cout << "Harl created" << std::endl;
-}
+Harl::Harl(){;}
 
-Harl::~Harl()
-{
-    std::cout << "Harl destroyed" << std::endl;
-}
+Harl::~Harl(){;}
 
 void Harl::debug(void)
 {
+	std::cout << "[DEBUG]" << std::endl;
     std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info(void)
 {
+	std::cout << "[INFO]" << std::endl;
     std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning(void)
 {
+	std::cout << "[WARNING]" << std::endl;
     std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error(void)
 {
+	std::cout << "[ERROR]" << std::endl;
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
--- ex06/main.cpp
+++ ex06/main.cpp
@@ -1,12 +1,44 @@
 #include "harl.hpp"
 
-int main()
+int get_ret(std::string str)
 {
-    Harl harl;
-    harl.complain("DEBUG");
-    harl.complain("INFO");
-    harl.complain("WARNING");
-    harl.complain("ERROR");
-    harl.complain("UNKNOWN");
+    if (std::string(str) == "DEBUG")
+        return 0;
+    else if (std::string(str) == "INFO")
+        return 1;
+    else if (std::string(str) == "WARNING")
+        return 2;
+    else if (std::string(str) == "ERROR")
+        return 3;
+    else
+        return -1;
+}
+
+int main(int argc, char **argv)
+{
+    if(argc != 2)
+    {
+        std::cout << "input Err" << std::endl;
+        return 1;
+    }
+
+    Harl    harl;
+    int     ret;
+
+    ret = get_ret(argv[1]);
+    switch (ret)
+    {
+		default:
+			std::cout << "[Probably complaining about insignificant problems]" << std::endl;
+			break;
+        case 0:
+            harl.complain("DEBUG");
+        case 1:
+            harl.complain("INFO");
+        case 2:
+            harl.complain("WARNING");
+        case 3:
+            harl.complain("ERROR");
+    }
     return 0;
 }
